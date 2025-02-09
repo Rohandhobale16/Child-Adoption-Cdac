5d2d0e50f8c53b14f04c5f319dd14b2420302db0
Anujrao204 <anujanilnemanwar204@gmail.com>
2025-02-09 14:34:58 +0530

    Added security for contactus

--- child_adoption_backend/src/main/java/com/app/security/SecurityConfiguration.java
+++ child_adoption_backend/src/main/java/com/app/security/SecurityConfiguration.java
@@ -14,55 +14,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration //equivalent to bean config xml
-@EnableWebSecurity //to enable annotation support for spring sec
+@Configuration // equivalent to bean config xml
+@EnableWebSecurity // to enable annotation support for spring sec
 public class SecurityConfiguration {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	@Autowired
 	private CustomJWTAuthenticationFilter customJWTAuthenticationFilter;
-	
-	
+
 	// Configure the bean to customize spring security filter chain
-		@Bean
-		public SecurityFilterChain authorizeRequests(HttpSecurity http) throws Exception
-		{
-			//1. Disable CSRF filter
-			http.csrf(customizer -> customizer.disable())
-			//2. configure URL based access
-	        .authorizeHttpRequests
-	        (request -> 
-	        request.requestMatchers("/Login",
-	        		"/addchildhome","/addparent","/geteventdetails","/getchildhomedetails","/childhome/allChildhomedetails").permitAll() 
-	        //required explicitly for JS clients (eg React app - to permit pre flight requests)
-	        .requestMatchers(HttpMethod.OPTIONS).permitAll()
-	        	
-	       .requestMatchers("/admin","/admin/{id}","/admin/deletechildhome/{id}","/admin/deleteperent/{id}")
-	       .hasRole("ADMIN")
-	       .requestMatchers("/employee/getrequestdetails","/employee/updaterequest/{id}","/employee/getemployeedetails/{id}","/employee/updateemployee")
-	       .hasRole("EMPLOYEE")
-	       .requestMatchers("/childhome/addchild","/childhome/addevents","/childhome/addemployee","/childhome/addrequest","/childhome/getchildhomedetails/{id}","/childhome/updatechildhome","/childhome/updaterequest/{id}")
-	       .hasRole("CHILDHOME")
-	       .requestMatchers("/parent")  
-	       .hasRole("PARENT")
-	       .anyRequest().authenticated())
-//	       .requestMatchers("/products/add","/products/delete")
-//	       .hasRole("ADMIN")        		.anyRequest().authenticated())  
-	  //      .httpBasic(Customizer.withDefaults()) - replacing it by custom JWT filter
-	        .sessionManagement(session 
-	        		-> session.sessionCreationPolicy(
-	        				SessionCreationPolicy.STATELESS));
-			//adding custom JWT fi;lter before any auth filter
-			http.addFilterBefore(customJWTAuthenticationFilter, 
-					UsernamePasswordAuthenticationFilter.class);
-	        return http.build();
-		}
-		//to supply Auth Mgr , configure it as a spring bean
-		@Bean
-		public AuthenticationManager authenticationManager
-		(AuthenticationConfiguration config) throws Exception
-		{
-			return config.getAuthenticationManager();
-		}
+	@Bean
+	public SecurityFilterChain authorizeRequests(HttpSecurity http) throws Exception {
+		// 1. Disable CSRF filter
+		http.csrf(customizer -> customizer.disable())
+				// 2. configure URL based access
+				.authorizeHttpRequests(request -> request.requestMatchers("/Login",
+						"/addchildhome", "/addparent", "/geteventdetails", "/getchildhomedetails",
+						"/childhome/allChildhomedetails", "contactus").permitAll()
+						// required explicitly for JS clients (eg React app - to permit pre flight
+						// requests)
+						.requestMatchers(HttpMethod.OPTIONS).permitAll()
+
+						.requestMatchers("/admin", "/admin/{id}", "/admin/deletechildhome/{id}",
+								"/admin/deleteperent/{id}")
+						.hasRole("ADMIN")
+						.requestMatchers("/employee/getrequestdetails", "/employee/updaterequest/{id}",
+								"/employee/getemployeedetails/{id}", "/employee/updateemployee")
+						.hasRole("EMPLOYEE")
+						.requestMatchers("/childhome/addchild", "/childhome/addevents", "/childhome/addemployee",
+								"/childhome/addrequest", "/childhome/getchildhomedetails/{id}",
+								"/childhome/updatechildhome", "/childhome/updaterequest/{id}")
+						.hasRole("CHILDHOME")
+						.requestMatchers("/parent")
+						.hasRole("PARENT")
+						.anyRequest().authenticated())
+				// .requestMatchers("/products/add","/products/delete")
+				// .hasRole("ADMIN") .anyRequest().authenticated())
+				// .httpBasic(Customizer.withDefaults()) - replacing it by custom JWT filter
+				.sessionManagement(session -> session.sessionCreationPolicy(
+						SessionCreationPolicy.STATELESS));
+		// adding custom JWT fi;lter before any auth filter
+		http.addFilterBefore(customJWTAuthenticationFilter,
+				UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+
+	// to supply Auth Mgr , configure it as a spring bean
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
 
 }
